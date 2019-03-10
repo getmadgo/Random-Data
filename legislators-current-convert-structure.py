308b4787d8cb37aff51893279a1df4d38de767ff
Ari Pollak <ajp@aripollak.com>
2019-03-10 15:37:17 -0400

    Add script to convert legislators-data to format that we want

---
+++ legislators-current-convert-structure.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import json
+from urllib.request import urlopen
+
+input = urlopen('https://theunitedstates.io/congress-legislators/legislators-current.json')
+input_array = json.loads(input.read())
+output_dict = {'current_legislators': {}}
+
+for legislator_dict in input_array:
+    official_name = legislator_dict['name']['official_full']
+    output_dict['current_legislators'][official_name] = legislator_dict
+
+output_file = open('legislators-by-name.json', 'w')
+json.dump(output_dict, output_file)
